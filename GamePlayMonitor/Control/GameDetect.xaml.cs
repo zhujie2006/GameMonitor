754e2effc3cee1e9fc02da2c2cfd48a7b76df0dc
zhujie2006 <zhujie.2006@163.com>
2014-10-18 15:54:04 +0800

    Change Styles Signed-off-by: zhujie2006 <zhujie.2006@163.com>

---
+++ GamePlayMonitor/Control/GameDetect.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GamePlayMonitor.Control
+{
+    /// <summary>
+    /// GameDetect.xaml 的交互逻辑
+    /// </summary>
+    public partial class GameDetect : UserControl
+    {
+        public GameDetect()
+        {
+            InitializeComponent();
+        }
+    }
+}
